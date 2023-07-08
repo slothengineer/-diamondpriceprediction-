cdbe6383551b0678592ba8e33897babe4ae627d5
slothengineer <slothenginner108@gmail.com>
2023-07-08 18:42:50 +0530

    Project completion

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,8 @@ import pandas as pd
 
 from src.exception import CustomException
 from src.logger import logging
+from sklearn.metrics import r2_score,mean_absolute_error,mean_squared_error
+
 
 def save_object(file_path,obj):
     try:
@@ -17,5 +19,40 @@ def save_object(file_path,obj):
             pickle.dump(obj,file_obj)
     
     except Exception as e:
+        raise CustomException(e,sys)
+    
+def evaluate_model(X_train,y_train,X_test,y_test,models):
+    try:
+        report = {}
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            
+            #model training
+            model.fit(X_train,y_train)
+
+            
+
+            #predicting Testing data
+            y_test_pred =model.predict(X_test)
+
+            #get R2 scores for train and test data
+            #train_model_score = r2_score(ytrain,y_train_pred)
+            test_model_score = r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]] =  test_model_score
+
+        return report
+    
+    except Exception as e:
+            logging.info('Exception occured during model training')
+            raise CustomException(e,sys)
+        
+        
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        logging.info('Exception Occured in load_object function utils')
         raise CustomException(e,sys)
     
